f390b3c1cda1eb970221cd560d54f8bbce07c49b
flow145 <268885866+flow145@users.noreply.github.com>
2026-06-02 18:01:42 +0500

    refactor: move env validation inside src

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { env } from '../env.ts'
 import { app } from './app.ts'
+import { env } from './env.ts'
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on http://localhost:${env.PORT}`)
