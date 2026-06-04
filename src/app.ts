e37b1337969821358a77b4243d083238a34f9eab
flow145 <268885866+flow145@users.noreply.github.com>
2026-06-04 16:40:07 +0500

    feat: add compression middleware

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
@@ -15,6 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(helmet())
+app.use(compression())
 app.use(cors({ origin: env.CORS_ORIGINS, credentials: true }))
 app.use(
   rateLimit({
